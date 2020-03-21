9a5ad31f9f13194cc943e5e3ee3362eaf85d1f74
Hassan-Qayyum <hassanqayyum8@gmail.com>
2020-03-21 19:03:03 +0500

    Blog app completed and portfolio project polished

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Blog
 
-# Create your views here.
+def allblogs(request):
+    allblogs = Blog.objects.all()
+    return render(request,"blog/allblogs.html",{"allblogs":allblogs})
+
+
+def detailblog(request, id):
+    blog = get_object_or_404(Blog, pk=id)
+    return render(request, 'blog/detailblog.html',{'blog':blog})
